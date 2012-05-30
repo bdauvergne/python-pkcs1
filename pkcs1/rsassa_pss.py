0ec477e90de5d55f87835e22f46895c6a21b2b14
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2012-05-30 09:17:09 +0200

    use helper method to compute bit size

--- pkcs1/rsassa_pss.py
+++ pkcs1/rsassa_pss.py
@@ -1,11 +1,9 @@
-import math
-
 import emsa_pss
 import primitives
 
 def sign(private_key, message,
         emsa_pss_encode=emsa_pss.encode):
-    mod_bits = int(math.ceil(math.log(private_key.n, 2)))
+    mod_bits = primitives.integer_bit_size(private_key.n)
     embits = mod_bits - 1
     em = emsa_pss_encode(message, embits)
     m = primitives.os2ip(em)
@@ -14,7 +12,7 @@ def sign(private_key, message,
 
 def verify(public_key, message, signature,
         emsa_pss_verify=emsa_pss.verify):
-    mod_bits = int(math.ceil(math.log(public_key.n, 2)))
+    mod_bits = primitives.integer_bit_size(public_key.n)
     s = primitives.os2ip(signature)
     m = public_key.rsavp1(s)
     embits = mod_bits - 1
