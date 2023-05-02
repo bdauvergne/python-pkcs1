2492f80b3a72aafd5b0045b5d40bd9adddc14d22
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2023-05-02 09:22:16 +0200

    Update version number to 0.9.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 import os
 
 setup(name='pkcs1',
-      version='0.9.6',
+      version='0.9.7',
       license='MIT',
       description='Python implementation of the RFC3447 or PKCS #1 version 2.0',
       url='https://github.com/bdauvergne/python-pkcs1',
